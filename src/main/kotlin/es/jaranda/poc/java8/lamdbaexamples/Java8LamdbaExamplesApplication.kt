216bb4edb0de0f467701561127a5c54eb1c87968
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2017-11-09 23:42:23 +0100

    Initial project creation

---
+++ src/main/kotlin/es/jaranda/poc/java8/lamdbaexamples/Java8LamdbaExamplesApplication.kt
@@ -0,0 +1,12 @@
+
+package es.jaranda.poc.java8.lamdbaexamples
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Java8LamdbaExamplesApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Java8LamdbaExamplesApplication::class.java, *args)
+}
---
+++ src/test/kotlin/es/jaranda/poc/java8/lamdbaexamples/Java8LamdbaExamplesApplicationTests.kt
@@ -0,0 +1,18 @@
+
+package es.jaranda.poc.java8.lamdbaexamples
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Java8LamdbaExamplesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+
+    }
+
+}
