06e7e8ccabc673bc5cedb167cd9458a9d6854ef3
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2017-11-10 00:10:15 +0100

    Creating LamdbaExampleUtil and self test

---
+++ src/main/kotlin/es/jaranda/poc/java8/lamdbaexamples/util/LambdaExampleUtil.java
@@ -0,0 +1,28 @@
+
+package es.jaranda.poc.java8.lamdbaexamples.util;
+
+import es.jaranda.poc.java8.lamdbaexamples.domain.Professor;
+import es.jaranda.poc.java8.lamdbaexamples.domain.Student;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class LambdaExampleUtil {
+
+    private static final int MAX_AGE = 25;
+
+    public List<String> obtainAllSubjectsFromStudentsMinorOf25(
+            final List<Student> students) {
+
+        return students.stream()
+                .filter( student -> student.getAge() < 25)
+                .flatMap(student -> student.getProfessors().stream())
+                .map(Professor::getSubject)
+                .distinct()
+                .collect(Collectors.toList());
+
+    }
+
+}
